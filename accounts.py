6b98097308727946d0626ea8da5362883cb9f868
John D. Fisher <fisherjd10@yahoo.com>
2015-10-03 09:29:53 -0700

    Check if QuickBooks is missing any accounts in the import .iif files

---
+++ accounts.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import csv
+
+__author__ = 'jfisher'
+
+QB_accounts = []
+with open('Book2.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        QB_accounts.append(row['Account'])
+
+gnc_accounts = []
+with open('liability.iif', 'wb') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        gnc_accounts.append(row['ACCNT'])
+with open('bank.iif', 'wb') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        gnc_accounts.append(row['ACCNT'])
+
+gnc_accounts = list(set(gnc_accounts))
+gnc_diff = []
+for item in QB_accounts:
+    if item not in gnc_accounts:
+        gnc_diff.append(item)
+gnc_diff = set(gnc_diff)
+for item in gnc_diff:
+    print(item)
